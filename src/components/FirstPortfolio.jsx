117bc52bbb5c0cdcd02df98de6f011f0236e0f38
Pana <vip_P4na@protonmail.com>
2023-08-12 11:48:47 +0200

    modifica text in my portfolio

--- src/components/FirstPortfolio.jsx
+++ src/components/FirstPortfolio.jsx
@@ -1,18 +1,26 @@
 import { Heading, Text } from "@chakra-ui/react";
 import React from "react";
 
-export const FirstPortfolio = () => {
+export const FirstPortfolio = (props) => {
   return (
     <>
       <Heading fontSize={48} pb={5}>
         🛠 My Portfolio
       </Heading>
-      <Text fontSize={24} pb={5} textAlign={"justify"}>
+      <Text
+        fontSize={props.isLargerThan1280 ? "2xl" : "xl"}
+        pb={5}
+        textAlign={"justify"}
+      >
         I dedicate my free time to creating personal projects, to practicing
         sports and experimenting with new technologies to stay constantly
         updated on the latest industry trends.
       </Text>
-      <Text fontSize={24} pb={10} textAlign={"justify"}>
+      <Text
+        fontSize={props.isLargerThan1280 ? "2xl" : "xl"}
+        pb={10}
+        textAlign={"justify"}
+      >
         I'm always looking for new ways to improve performance and the
         efficiency of the projects I work on, using the most technologies
         advances and best development practices.
