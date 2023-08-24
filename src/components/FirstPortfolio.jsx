5ee4e2e902992a7c62b7d5d8bb3acf822faaee3c
Vincenzo <vip_p4na@protonmail.com>
2023-08-24 12:11:37 +0200

    fix text cards

--- src/components/FirstPortfolio.jsx
+++ src/components/FirstPortfolio.jsx
@@ -8,13 +8,13 @@ export const FirstPortfolio = (props) => {
       </Heading>
       <Stack color={props.isDark ? "gray" : "black"}>
         <Text fontSize={"xl"} pb={5}>
-          I dedicate my free time to creating personal projects, to practicing
-          sports and experimenting with new technologies to stay constantly
+          I dedicate my free time to creating personal projects, practicing
+          sports, and experimenting with new technologies to stay constantly
           updated on the latest industry trends.
         </Text>
         <Text fontSize={"xl"} pb={10}>
           I'm always looking for new ways to improve performance and the
-          efficiency of the projects I work on, using the most technologies
+          efficiency of the projects I work on, using the most technological
           advances and best development practices.
         </Text>
       </Stack>
