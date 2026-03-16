8d42897a4ab0c75645491791c3200a4e14fa4281
Vincenzo Panacciulli <vincenzo.panacciulli@weroad.it>
2026-03-16 20:28:22 +0100

    feat: support typescript

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,5 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -8,11 +8,11 @@ export default defineConfig({
     rollupOptions: {
       output: {
         manualChunks(id) {
-          if (id.includes("node_modules")) {
+          if (id.includes('node_modules')) {
             return id
               .toString()
-              .split("node_modules/")[1]
-              .split("/")[0]
+              .split('node_modules/')[1]
+              .split('/')[0]
               .toString();
           }
         },
